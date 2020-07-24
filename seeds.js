58667c0ea89c55f19657ab907010f94c4dcda8ab
Neha-Salian <56082412+Neha-Salian@users.noreply.github.com>
2020-07-24 13:27:19 +0530

    Add files via upload

---
+++ seeds.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Clouds Rest is a mountain in Yosemite National Park east northeast of Yosemite Village, California."
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "A mesa is an isolated, flat-topped elevation, ridge or hill, which is bounded from all sides by steep escarpments and stands distinctly above a surrounding plain."
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "No Europeans visited the canyon again for more than two hundred years."
+    }
+]
+
+function seedDB(){
+   //Remove all campgrounds
+   Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        
+    }); 
+   
+}
+
+module.exports = seedDB;
+ 
